2fb37dbb006f7d5b43dfd2e0b0e98ef2d8cae6c6
Nithish199 <91787383+Nithish199@users.noreply.github.com>
2021-10-28 12:38:29 +0530

    Add files via upload

---
+++ ActionSelectable.java
@@ -0,0 +1,38 @@
+package Week4.Day2.Assignments;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ActionSelectable {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://jqueryui.com/selectable");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+		WebElement frame = driver.findElement(By.xpath("//iframe"));
+		driver.switchTo().frame(frame);
+		
+		WebElement item1 = driver.findElement(By.xpath("//li[text()='Item 1']"));
+		WebElement item2 = driver.findElement(By.xpath("//li[text()='Item 2']"));
+		WebElement item3 = driver.findElement(By.xpath("//li[text()='Item 3']"));
+		WebElement item4 = driver.findElement(By.xpath("//li[text()='Item 4']"));
+		
+		Actions builder=new Actions(driver);
+		builder.clickAndHold(item1).moveToElement(item4).release().perform();
+		
+		
+
+		
+		
+		
+	}
+
+}
